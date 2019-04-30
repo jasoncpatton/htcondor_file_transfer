fdf263fe2c2d3769f538e81d8fe56a48c0351824
Brian Bockelman <bbockelm@cse.unl.edu>
2019-04-30 09:57:07 -0500

    Consolidate all executables into a single one.

--- xfer.py
+++ xfer.py
@@ -9,9 +9,11 @@ from __future__ import print_function, unicode_literals
 
 import argparse
 import errno
+import hashlib
 import logging
 import os
 import sys
+import time
 
 import htcondor
 
@@ -23,7 +25,7 @@ JOB CalculateWork calc_work.sub DIR calc_work
 
 # Gather the local file listing and write out the sub-DAG
 # to perform the actual transfers
-SCRIPT POST CalculateWork {exec_py} write_subdag {source_prefix} source_manifest.txt {dest_prefix} destination_manifest.txt
+SCRIPT POST CalculateWork {exec_py} write_subdag {source_prefix} source_manifest.txt {dest_prefix} destination_manifest.txt {other_args}
 
 SUBDAG EXTERNAL DoXfers calc_work/do_work.dag
 
@@ -51,7 +53,7 @@ executable = {}
 output = $(src_file_noslash).out
 error = $(src_file_noslash).err
 log = xfer_file.log
-arguments = $(src_file)
+arguments = exec $(src_file)
 should_transfer_files = YES
 transfer_output_files = file0, metadata
 transfer_output_remaps = "file0 = $(dst_file); metadata = $(src_file_noslash).metadata"
@@ -62,7 +64,8 @@ queue
 DO_WORK_DAG_HEADER = \
 """
 CATEGORY ALL_NODES TRANSFER_JOBS
-MAXJOBS TRANSFER_JOBS 1
+{}
+
 """
 
 DO_WORK_DAG_SNIPPET = \
@@ -71,7 +74,7 @@ JOB xfer_{name} xfer_file.sub DIR calc_work
 VARS xfer_{name} src_file_noslash="{src_file_noslash}"
 VARS xfer_{name} src_file="{src_file}"
 VARS xfer_{name} dst_file="{dest}"
-SCRIPT POST xfer_{name} {xfer_verify} {dest} {src_file_noslash}.metadata transfer_manifest.txt
+SCRIPT POST xfer_{name} {xfer_py} verify {dest} {src_file_noslash}.metadata transfer_manifest.txt
 
 """
 
@@ -104,6 +107,16 @@ def parse_args():
     parser_subdag.add_argument("source_manifest")
     parser_subdag.add_argument("dest_prefix")
     parser_subdag.add_argument("dest_manifest")
+    parser_subdag.add_argument("--test-mode", help="Testing mode (only transfers small files)",
+        default=False, action="store_true", dest="test_mode")
+
+    parser_exec = subparsers.add_parser("exec")
+    parser_exec.add_argument("src")
+
+    parser_verify = subparsers.add_parser("verify")
+    parser_verify.add_argument("dest")
+    parser_verify.add_argument("metadata")
+    parser_verify.add_argument("metadata_summary")
  
     return parser.parse_args()
 
@@ -131,7 +144,7 @@ def submit_parent_dag(working_dir, source_dir, dest_dir, test_mode=False):
 
     with open(os.path.join(working_dir, "xfer.dag"), "w") as fd:
         fd.write(PARENT_DAG.format(exec_py=full_exec_path, source_prefix=source_dir,
-            dest_prefix=dest_dir))
+            dest_prefix=dest_dir, other_args="--test-mode" if test_mode else ""))
 
     with open(os.path.join(working_dir, "calc_work", "calc_work.sub"), "w") as fd:
         fd.write(CALC_WORK_JOB.format(exec_py=full_exec_path, source_dir=source_dir,
@@ -192,7 +205,7 @@ def parse_manifest(prefix, manifest, log_name):
     return files
 
 
-def write_subdag(source_prefix, source_manifest, dest_prefix, dest_manifest):
+def write_subdag(source_prefix, source_manifest, dest_prefix, dest_manifest, test_mode=False):
     src_files = parse_manifest(source_prefix, source_manifest, "Source")
 
     generate_file_listing(dest_prefix, "destination_manifest.txt")
@@ -202,20 +215,16 @@ def write_subdag(source_prefix, source_manifest, dest_prefix, dest_manifest):
     dest_files_set = set(dest_files)
     files_to_xfer = src_files_set - dest_files_set
 
-    # TODO: xfer_exec.py should be merged into this as well...
-    dirpath = os.path.split(sys.argv[0])[0]
-    full_exec_path = os.path.join(dirpath, "xfer_exec.py")
+    info = os.path.split(sys.argv[0])
+    full_exec_path = os.path.join(os.path.abspath(info[0]), info[1])
 
     with open("xfer_file.sub", "w") as fp:
         fp.write(XFER_FILE_JOB.format(full_exec_path))
 
-    full_exec_path = os.path.join(dirpath, "xfer_verify.py")
     idx = 0
-
     dest_dirs = set()
-
     with open("do_work.dag", "w") as fp:
-        fp.write(DO_WORK_DAG_HEADER)
+        fp.write(DO_WORK_DAG_HEADER.format("MAXJOBS TRANSFER_JOBS 1" if test_mode else ""))
         files_to_xfer = list(files_to_xfer)
         files_to_xfer.sort()
         for fname in files_to_xfer:
@@ -226,7 +235,7 @@ def write_subdag(source_prefix, source_manifest, dest_prefix, dest_manifest):
             dest_dirs.add(os.path.split(dest)[0])
             logging.info("File transfer to perform: %s->%s", src_file, dest)
             fp.write(DO_WORK_DAG_SNIPPET.format(name=idx, src_file=src_file,
-                xfer_verify=full_exec_path, src_file_noslash=src_file_noslash, dest=dest))
+                xfer_py=full_exec_path, src_file_noslash=src_file_noslash, dest=dest))
 
     for dest_dir in dest_dirs:
         try:
@@ -234,6 +243,94 @@ def write_subdag(source_prefix, source_manifest, dest_prefix, dest_manifest):
         except OSError as oe:
             if oe.errno != errno.EEXIST:
                 raise
+
+
+def xfer_exec(src):
+    logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
+
+    if '_CONDOR_JOB_AD' not in os.environ:
+        print("This executable must be run within the HTCondor runtime environment.")
+        sys.exit(1)
+
+    logging.info("About to copy %s to file0", src)
+    src_fd = open(src, "r")
+    dest_fd = open("file0", "w")
+    file_size = os.fstat(src_fd.fileno()).st_size
+    logging.info("There are %.2fMB to copy", file_size / 1024. / 1024.)
+    last_log = time.time()
+    buf = src_fd.read(1024*1024)
+    hash_obj = hashlib.sha1()
+    byte_count = len(buf)
+    while len(buf) > 0:
+        hash_obj.update(buf)
+        dest_fd.write(buf)
+        buf = src_fd.read(1024*1024)
+        now = time.time()
+        if (now - last_log > 5):
+            logging.info("Copied %.2f of %.2fMB; %.1f%% done", byte_count / 1024. / 1024., file_size / 1024. / 1024.,
+                (byte_count / float(file_size)) * 100)
+            last_log = now
+        byte_count += len(buf)
+
+    src_fd.close()
+    logging.info("Copy complete; about to synchronize file to disk")
+    os.fsync(dest_fd.fileno())
+    logging.info("File synchronized to disk")
+    dest_fd.close()
+
+    logging.info("File metadata: hash=%s, size=%d", hash_obj.hexdigest(), byte_count)
+    with open("metadata", "w") as metadata_fd:
+        metadata_fd.write("{} {} {}\n".format(src, hash_obj.hexdigest(), byte_count).encode('utf-8'))
+
+
+def verify(dest, metadata, metadata_summary):
+    with open(metadata, "r") as metadata_fd:
+        if os.fstat(metadata_fd.fileno()).st_size > 16384:
+            logging.error("Metadata file is too large")
+            sys.exit(1)
+        contents = metadata_fd.read()
+        info = contents.strip().split()
+        if len(info) != 3:
+            logging.error("Metadata file format incorrect")
+            sys.exit(1)
+        src_fname = info[0].decode('utf-8')
+        src_hexdigest = info[1]
+        src_size = int(info[2])
+
+    logging.info("About to verify contents of %s", dest)
+    dest_fd = open(dest, "r")
+    dest_size = os.fstat(dest_fd.fileno()).st_size
+    if src_size != dest_size:
+        logging.error("Copied file size (%d bytes) does not match source file size (%d bytes)", dest_size, src_size)
+        sys.exit(2)
+    logging.info("There are %.2fMB to verify", dest_size / 1024. / 1024.)
+    last_log = time.time()
+    buf = dest_fd.read(1024*1024)
+    hash_obj = hashlib.sha1()
+    byte_count = len(buf)
+    while len(buf) > 0:
+        hash_obj.update(buf)
+        buf = dest_fd.read(1024*1024)
+        now = time.time()
+        if now - last_log > 5:
+            logging.info("Verified %.2f of %.2fMB; %.1f%% done", byte_count / 1024. / 1024., dest_size / 1024. / 1024.,
+                (byte_count / float(dest_size)) * 100)
+            last_log = now
+        byte_count += len(buf)
+
+    dest_fd.close()
+    if src_hexdigest != hash_obj.hexdigest():
+        logging.info("Destination file (%s) has incorrect SHA1 digest of %s, which does not match"
+            " source file %s (digest %s)", dest, src_hexdigest, src_fname, hash_obj.hexdigest())
+        sys.exit(1)
+    else:
+        logging.info("File verification successful: Destination (%s) and source (%s) have matching"
+            " SHA1 digest (%s)", dest, src_fname, src_hexdigest)
+
+    with open(metadata_summary, "a") as md_fd:
+        md_fd.write("{} {} {}\n".format(src_fname, src_hexdigest, src_size).encode('utf-8'))
+        os.fsync(md_fd.fileno())
+
 
 def main():
     logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
@@ -250,7 +347,11 @@ def main():
         generate_file_listing(args.src, "source_manifest.txt", test_mode=args.test_mode)
     elif args.cmd == "write_subdag":
         logging.info("Generating SUBGDAG for transfer of %s->%s", args.source_prefix, args.dest_prefix)
-        write_subdag(args.source_prefix, args.source_manifest, args.dest_prefix, args.dest_manifest)
+        write_subdag(args.source_prefix, args.source_manifest, args.dest_prefix, args.dest_manifest, test_mode=args.test_mode)
+    elif args.cmd == "exec":
+        xfer_exec(args.src)
+    elif args.cmd == "verify":
+        verify(args.dest, args.metadata, args.metadata_summary)
 
 if __name__ == '__main__':
     main()
